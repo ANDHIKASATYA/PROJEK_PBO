2cf5b0ca750dd6fa1ad63192e620096fa365c36d
ANDHIKASATYA <101849336+ANDHIKASATYA@users.noreply.github.com>
2022-12-24 19:36:19 +0700

    Delete Awal.java

--- Awal.java
+++
@@ -1,7 +0,0 @@
-public abstract class Awal {
-    public abstract void Masuk();
-    public abstract void hargaDslr();
-    public abstract void hargaAnalog();
-    public abstract void outputDslr();
-    public abstract void outputAnalog();
-}
